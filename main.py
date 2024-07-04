34c42a1777e505b9fadc46abd96b5c385b7ec5b8
Steddman Bell <steddfast@gmail.com>
2024-07-04 13:55:12 -0700

    response variable created for posting json to openai

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ client = OpenAI(
   organization='org-H62SnBA9zVxUyTrKWWaGcFHs',
   project='proj_YmL1LLCERSFKkexPHujxpfij'
 )
+
+response = client.completions.create(
+  model="gpt-3.5-turbo",
+  prompt="variable goes here to allow post requests from rails",
+  max_tokens=100,
+  user="user_id variable"
+)
