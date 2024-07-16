6479a3493ce1e18f5d002e9340fc5c0f9f01e7f6
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-07-15 22:12:26 -0400

    Configured a dockerfile, build and push docker image, updates fly.io configuration, deployed to fly.io, verified deployment

--- app/main.py
+++ app/main.py
@@ -62,3 +62,7 @@ async def generate_advice(request: Request):
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
